c6fe3daf304267cff8b1721f608dde41c8839b14
Max Luebbering <max.luebbering@gmail.com>
2019-09-16 00:16:31 +0200

    Added init files and setup.py

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+
+     name='advattack',
+
+     version='0.1',
+
+     author="Max Luebbering",
+
+     author_email="max.luebbering@gmail.com",
+
+     description="A framework to run adversarial attacks",
+
+     url="https://github.com/le1nux/advattack",
+
+     packages=setuptools.find_packages()
+ )

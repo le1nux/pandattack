fe171665260a698ebd5a3004cb429e39b983ab3e
Max Luebbering <max.luebbering@gmail.com>
2019-09-16 10:45:25 +0200

    added exception handling

---
+++ src/advattack/error_handling/exception.py
@@ -0,0 +1,11 @@
+class DatasetError(Exception):
+    """Exception raised for errors in dataset instances.
+
+    Attributes:
+        expression -- input expression in which the error occurred
+        message -- explanation of the error
+    """
+
+    def __init__(self, expression, message):
+        self.expression = expression
+        self.message = message

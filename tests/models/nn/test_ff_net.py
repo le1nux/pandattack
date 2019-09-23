2e7c3b8c83d2745a1bb7674eecebd11deda9de77
Max Luebbering <max.luebbering@gmail.com>
2019-09-23 21:19:36 +0200

    Nan fix in test_ff_net.py

--- tests/models/nn/test_ff_net.py
+++ tests/models/nn/test_ff_net.py
@@ -5,10 +5,8 @@ import shutil
 from advattack.models.nn.ff_net import  FFModel
 import torch
 import torch.nn as nn
-import os
 from torch.utils.data import BatchSampler, SubsetRandomSampler
 from advattack.data_handling.mnist.mnist_dataset import MNISTDataset
-from advattack import datasets_path
 from advattack.data_handling.dataset_loader import DatasetLoader
 from torchvision import transforms
 import numpy as np
@@ -29,18 +27,19 @@ class TestFFModel:
 
     @pytest.fixture
     def mnist_dataset(self, mnist_dataset_path):
-        dataset = MNISTDataset.load(mnist_dataset_path)
-        yield dataset
-
-    @pytest.fixture
-    def train_valid_loader(self, mnist_dataset):
-        batch_size = 30
 
         # generate training set
         feature_transform_fun = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,))
         ])
+
+        dataset = MNISTDataset.load(mnist_dataset_path, feature_transform_fun=feature_transform_fun)
+        yield dataset
+
+    @pytest.fixture
+    def train_valid_loader(self, mnist_dataset):
+        batch_size = 100
 
         train_indices, valid_indices = mnist_dataset.get_train_and_validation_set_indices(train_valid_split_ratio=0.8, seed=2)
         train_loader = DatasetLoader(mnist_dataset, batch_sampler=BatchSampler(sampler=SubsetRandomSampler(train_indices),
@@ -54,14 +53,10 @@ class TestFFModel:
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print("Device: " + str(device))
 
-        layers = np.array([28 * 28, 250, 250, 250, 10]).flatten()
-
-        learning_rate = 0.2
-        epochs = 1
+        model_config = {"layers": np.array([28 * 28, 250, 250, 250, 10]).flatten().tolist()}
 
         # instantiate model
-        loss_function = nn.NLLLoss()
-        model = FFModel(layers, loss_function=loss_function).to(device)
+        model = FFModel(**model_config).to(device)
         yield model
 
     def test_train_model(self, train_valid_loader, ff_net_model):
@@ -69,6 +64,7 @@ class TestFFModel:
         # compare them to the ones calculated after one epoch. If they are different
         # we assume model is learning something. Fully training a model is not feasible
         # for testing in a CI environment.
+        loss_function = nn.NLLLoss()
 
         train_loader, valid_loader = train_valid_loader
 
@@ -77,8 +73,8 @@ class TestFFModel:
         for layer_key, module in initial_module_dict.items():
             init_weight_dict[layer_key] = initial_module_dict["0"].weight.data.clone()
 
-        optimizer = torch.optim.SGD(ff_net_model.parameters(), lr=0.1)
-        ff_net_model.train_model(train_loader=train_loader, valid_loader=valid_loader, optimizer=optimizer, epochs=1)
+        optimizer = torch.optim.SGD(ff_net_model.parameters(), lr=0.0008)
+        ff_net_model.train_model(train_loader=train_loader, valid_loader=valid_loader, optimizer=optimizer, loss_function=loss_function, epochs=1)
 
         trained_module_dict = ff_net_model.fc_layers._modules
         weights_different = True

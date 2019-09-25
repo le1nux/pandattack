8350662a6c7e6443f036472de23bde45f967c49c
Max Luebbering <max.luebbering@gmail.com>
2019-09-25 18:34:17 +0200

    Renamed get_data_type and get_model_type to get_data_identifier and get_model_identifier

--- src/advattack/data_handling/dataset_loader.py
+++ src/advattack/data_handling/dataset_loader.py
@@ -1,15 +1,11 @@
-from torch.utils.data import DataLoader
 import torch
-import os
-from torch.utils.data import DataLoader, BatchSampler, SubsetRandomSampler
-from advattack.data_handling.mnist.mnist_dataset import MNISTDataset
-from advattack import datasets_path
+from torch.utils.data import DataLoader
+from advattack.data_handling.dataset import Dataset
+
 
 class DatasetLoader(DataLoader):
-
-    def __init__(self, dataset, batch_sampler=None, collate_fn=None):
+    def __init__(self, dataset: Dataset, batch_sampler=None, collate_fn=None):
         """
-
         :param dataset:
         :param batch_size:
         :param shuffle:
@@ -33,14 +29,5 @@ class DatasetLoader(DataLoader):
         # mapping = [item[2] for item in batch]
         return [inputs, inputs_len, targets_tensor]
 
-
-if __name__ == "__main__":
-    mnist_path = os.path.join(datasets_path, "mnist")
-    dataset = MNISTDataset.load(mnist_path)
-    train_indices, valid_indices = dataset.get_train_and_validation_set_indices(train_valid_split_ratio=0.8, seed=2)
-    train_loader = DatasetLoader(dataset, batch_sampler=BatchSampler(sampler=SubsetRandomSampler(train_indices),
-                                                                  batch_size=50, drop_last=False))
-    valid_loader = DatasetLoader(dataset, batch_sampler=BatchSampler(sampler=SubsetRandomSampler(valid_indices),
-                                                                  batch_size=50, drop_last=False))
-
-    print(f"length of training set: {len(train_loader)}, length of validation set: {len(valid_loader)}")
+    def get_dataset_identifier(self) -> str:
+        return self.dataset.get_dataset_identifier()
--- src/advattack/models/model_repository.py
+++ src/advattack/models/model_repository.py
@@ -12,19 +12,19 @@ class ModelRepository:
         if device is None:
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-        model_folder = os.path.join(models_path, dataset_class.get_dataset_type(), model_class.get_model_type())
+        model_folder = os.path.join(models_path, dataset_class.get_dataset_identifier(), model_class.get_model_identifier())
         config_path = os.path.join(model_folder, "config.json")
 
         with open(config_path, "r") as f:
             model_config = json.load(f)
         model = model_class(**model_config).to(device)
-        model_path = os.path.join(models_path, dataset_class.get_dataset_type(), model.get_model_type())
+        model_path = os.path.join(models_path, dataset_class.get_dataset_identifier(), model.get_model_identifier())
         model.load_model(model_path)
         return model
 
     @staticmethod
     def store_model(model: NNModel, dataset_class) -> str:
-        model_folder = os.path.join(models_path, dataset_class.get_dataset_type(), model.get_model_type())
+        model_folder = os.path.join(models_path, dataset_class.get_dataset_identifier(), model.get_model_identifier())
         config_path = os.path.join(model_folder, "config.json")
         model.save_model(folder_path=model_folder)
         with open(config_path, "w") as f:

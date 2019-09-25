386a90b5e6230504b6a921fc3943029661063eb8
Max Luebbering <max.luebbering@gmail.com>
2019-09-25 18:38:29 +0200

    Some minor changes

--- src/advattack/attacks/attack.py
+++ src/advattack/attacks/attack.py
@@ -3,31 +3,34 @@ from advattack.models.nn.net import NNModel
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 class Attack:
-    def __init__(self, model:NNModel):
+    def __init__(self, model:NNModel, device):
         self.model = model
+        self.device = device
 
     @abstractmethod
-    def attack(self, original_image):
+    def attack(self, original_image, original_target):
         raise NotImplementedError
 
 
 class AttackResult:
     def __init__(self, original_image, original_target, original_confidence, adversarial_image, adversarial_target, adversarial_confidence, gradient_image):
+        # TODO we need a custom reshape function here, that is passed as an argument to the function
         shape = np.sqrt(original_image[0].size()).astype(np.int32).repeat(2)
         self.original_image = original_image.view(*shape)
         self.original_target = int(original_target)
-        self.original_confidence = float(original_confidence)
+        self.original_confidence = np.e**float(original_confidence)
         self.adversarial_image = adversarial_image.view(*shape)
         self.adversarial_target = int(adversarial_target)
-        self.adversarial_confidence = float(adversarial_confidence)
+        self.adversarial_confidence = np.e**float(adversarial_confidence)
         self.gradient_image = gradient_image.view(*shape)
 
     def visualize(self):
         fig = plt.figure()
         fig.add_subplot(1, 3, 1)
         plt.imshow(self.original_image, cmap='gray')
-        plt.title(f'Original\nTarget and prediction:{self.original_target}\n ln conf.:{round(self.original_confidence, 5)}')
+        plt.title(f'Original\nTarget and prediction:{self.original_target}\nConf.:{round(self.original_confidence, 5)}')
 
         fig.add_subplot(1, 3, 2)
         plt.imshow(self.gradient_image, cmap='gray')
@@ -35,6 +38,6 @@ class AttackResult:
 
         fig.add_subplot(1, 3, 3)
         plt.imshow(self.adversarial_image, cmap='gray')
-        plt.title(f'Adversarial Example\nPrediction:{self.adversarial_target}\nln conf.:{round(self.adversarial_confidence, 5)}')
+        plt.title(f'Adversarial Example\nPrediction:{self.adversarial_target}\nConf.:{round(self.adversarial_confidence, 5)}')
 
         plt.show()
--- src/advattack/attacks/white_box/fgsm.py
+++ src/advattack/attacks/white_box/fgsm.py
@@ -14,23 +14,24 @@ from advattack.models.model_repository import ModelRepository
 
 
 class FGSM(Attack):
-    def __init__(self, model: NNModel):
-        super(FGSM, self).__init__(model)
+    def __init__(self, model: NNModel, device, epsilon=0.25, ):
+        super(FGSM, self).__init__(model, device)
+        self.epsilon = epsilon
 
-    def calc_adversarial(self, original_image, epsilon, data_grad):
+    def calc_adversarial(self, original_image, data_grad):
         # Collect the element-wise sign of the data gradient
         sign_data_grad = data_grad.sign()
         # Create the perturbed image by adjusting each pixel of the input image
-        gradient_image = epsilon*sign_data_grad
+        gradient_image = self.epsilon*sign_data_grad
         perturbed_image = original_image + gradient_image
         # Adding clipping to maintain [0,1] range
         perturbed_image = torch.clamp(perturbed_image, 0, 255)
         # Return the perturbed image
         return perturbed_image, gradient_image
 
-    def search_for_adversarial_example(self, original_image, original_target, epsilon):
+    def attack(self, original_image, original_target):
         # Send the data and label to the device
-        data, original_target = original_image.to(device), original_target.to(device)
+        data, original_target = original_image.to(self.device), original_target.to(self.device)
         # Set requires_grad attribute of tensor. Important for Attack
         data.requires_grad = True
         # Forward pass the data through the model
@@ -50,7 +51,7 @@ class FGSM(Attack):
         # Collect datagrad
         data_grad = data.grad.data
         # Call FGSM Attack
-        adversarial_image, gradient_image = self.calc_adversarial(data, epsilon, data_grad)
+        adversarial_image, gradient_image = self.calc_adversarial(data, data_grad)
          # Re-classify the perturbed image
         output = self.model(adversarial_image)
         adversarial_target = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
@@ -84,11 +85,18 @@ if __name__ == "__main__":
     dataset = MNISTDataset.load(mnist_path, feature_transform_fun=feature_transform_fun)
     dataset_loader = DatasetLoader(dataset)
 
-    fgsm = FGSM(model=model)
+    fgsm = FGSM(model=model, epsilon=1, device=device)
     iterator = iter(dataset_loader)
-    original_image, _, original_target = next(iterator)
-    attack_result = fgsm.search_for_adversarial_example(original_image, original_target, epsilon=100)
-    attack_result.visualize()
+    for idx, (original_image, _, original_target) in enumerate(iterator):
+        print(idx)
+        try:
+            attack_result = fgsm.attack(original_image, original_target)
+            attack_result.visualize()
+        except AdversarialNotFoundError:
+            pass
+        except AttackError:
+            pass
+
 
 
 
